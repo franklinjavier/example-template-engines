e19ed4d356b32e7120eadd6daee943903030e571
Franklin Javier <franklinjalves@gmail.com>
2015-10-24 22:18:38 -0200

    First commit

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-example-template-engines-2014
-=============================
+example-template-engines
+=========================
 
+### I made some changes to request real data before render to the view.
+
+>
 This example refers to the article on [strongloop.com](http://) and [npmawesome.com](http://npmawesome.com/) about the state of JavaScript templating engines for 2014. You can play around with the [runnable](http://runnable.com/VFhUDXI1zuRccOxd/javascript-templating-engines-2014-for-node-js-jade-ejs-mustache-nunjucks-npmawesome-and-strongloop) example.
 
 ## Usage
 
-    git clone https://github.com/npmawesome/example-template-engines-2014.git
-    cd example-template-engines-2014
+    git clone https://github.com/franklinjavier/example-template-engines.git
+    cd example-template-engines
     npm install
     npm test
--- index.js
+++ index.js
@@ -6,77 +6,72 @@ var mustache = require('mustache');
 var dust = require('dustjs-linkedin');
 var ejs = require('ejs');
 var nunjucks = require('nunjucks');
+var axios = require('axios');
+var request = require('request');
+var oboe = require('oboe');
+var compression = require('compression');
+var bodyParser = require('body-parser');
 
+var API = 'https://www.reddit.com/subreddits/search.json?limit=100&q=lol';
 var app = express();
+var config = {
+  url: API,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
 
+function getData(cb) {
+  request(config, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      var locals = {todos: info.data.children};
+      cb(locals);
+    }
+  })
+}
+
 app.get('/jade', function(req, res) {
-  var options = {todos: todos};
-  res.send(jade.renderFile('./jade/index.jade', options));
-})
+  getData(function(locals) {
+    res.send(jade.renderFile('./jade/index.jade', locals));
+  });
+});
 
 app.get('/mustache', function(req, res) {
   var template = fs.readFileSync('./mustache/index.mustache', 'utf8');
   var partials = {
     item: fs.readFileSync('./mustache/item_partial.mustache', 'utf8')
   };
-  var locals = {todos: todos};
-  res.send(mustache.render(template, locals, partials));
-});
 
-app.get('/dust', function(req, res) {
-  function register(name) {
-    var template = dust.compile(fs.readFileSync('./dust/' + name + '.dust', 'utf8'), name);
-    dust.loadSource(template);
-  }
-
-  register('index');
-  register('layout');
-  register('item_partial');
-
-  var delay = 1;
-
-  var locals = {
-    todos: todos,
-    stream: function(chunk, context, bodies) {
-      return chunk.map(function(chunk) {
-        setTimeout(function() {
-          chunk
-            .render(bodies.block, context)
-            .end();
-        }, delay++ * 1000);
-      });
-    }
-  };
-
-  dust
-    .stream('index', locals)
-    .on('error', function(err) {
-      console.log(err.message);
-      res.send(err.message);
-    })
-    .pipe(res)
-    ;
+  getData(function(locals) {
+    res.send(mustache.render(template, locals, partials));
+  });
 });
 
 app.get('/nunjucks', function(req, res) {
-  var locals = {todos: todos};
-  nunjucks.render('./nunjucks/index.html', locals, function(err, result) {
-    if(err) {
-      return res.send(err.message);
-    }
-    res.send(result);
+  getData(function(locals) {
+    nunjucks.render('./nunjucks/index.html', locals, function(err, result) {
+      if(err) {
+        return res.send(err.message);
+      }
+      res.send(result);
+    });
   });
+
 });
 
 app.get('/ejs', function(req, res) {
   var template = fs.readFileSync('./ejs/index.ejs', 'utf8');
-  var options = {
-    filename: './ejs/index.ejs',
-    todos: todos
-  };
-  res.send(ejs.render(template, options));
+  getData(function(locals) {
+    locals.filename = './ejs/index.ejs';
+    res.send(ejs.render(template, locals));
+  });
 });
 
 app.listen(3000);
